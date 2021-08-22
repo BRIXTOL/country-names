8df09d3b0ad2bc25ee8185873c68484f08c0c0eb
Nikolas Savvidis <nicos.savidis@gmail.com>
2021-08-23 01:52:06 +0200

    provide test case for exposed Countries mapping

--- test/index.js
+++ test/index.js
@@ -1,32 +1,50 @@
-import test from "ava";
-import { getCountryName } from "../package/index";
+import test from 'ava';
+import { getCountryName, Countries } from '../package/index';
 
-test("Country code", (t) => {
-  t.is(getCountryName("SE"), "Sweden");
-  t.is(getCountryName("Nl"), "Netherlands");
-  t.is(getCountryName("dE"), "Germany");
-  t.is(getCountryName("us"), "United States of America");
-  t.is(getCountryName("AU"), "Australia");
+test('Country code', (t) => {
 
-  t.pass();
-});
-
-test("Country code in uppercase", (t) => {
-  t.is(getCountryName("SE"), "Sweden");
-  t.is(getCountryName("NL"), "Netherlands");
-  t.is(getCountryName("DE"), "Germany");
-  t.is(getCountryName("US"), "United States of America");
-  t.is(getCountryName("AU"), "Australia");
+  t.is(getCountryName('SE'), 'Sweden');
+  t.is(getCountryName('Nl'), 'Netherlands');
+  t.is(getCountryName('dE'), 'Germany');
+  t.is(getCountryName('us'), 'United States of America');
+  t.is(getCountryName('AU'), 'Australia');
 
   t.pass();
 });
 
-test("Country code in lowercase", (t) => {
-  t.is(getCountryName("se"), "Sweden");
-  t.is(getCountryName("nl"), "Netherlands");
-  t.is(getCountryName("de"), "Germany");
-  t.is(getCountryName("us"), "United States of America");
-  t.is(getCountryName("au"), "Australia");
+test('Country code in uppercase', (t) => {
+
+  t.is(getCountryName('SE'), 'Sweden');
+  t.is(getCountryName('NL'), 'Netherlands');
+  t.is(getCountryName('DE'), 'Germany');
+  t.is(getCountryName('US'), 'United States of America');
+  t.is(getCountryName('AU'), 'Australia');
 
   t.pass();
+
+});
+
+test('Country code in lowercase', (t) => {
+
+  t.is(getCountryName('se'), 'Sweden');
+  t.is(getCountryName('nl'), 'Netherlands');
+  t.is(getCountryName('de'), 'Germany');
+  t.is(getCountryName('us'), 'United States of America');
+  t.is(getCountryName('au'), 'Australia');
+
+  t.pass();
+
+});
+
+test('Countries mapping export', (t) => {
+
+  t.is(Countries.SE, 'Sweden');
+  t.is(Countries.NL, 'Netherlands');
+  t.is(Countries.DE, 'Germany');
+  t.is(Countries.US, 'United States of America');
+  t.is(Countries.AU, 'Australia');
+  t.is(Countries.RU, 'Russian Federation');
+
+  t.pass();
+
 });
